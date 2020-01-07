526151802de8f3067b28c064123425b229772b0c
Théotime F <42675516+theotime-me@users.noreply.github.com>
2020-01-07 12:56:58 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,46 @@ let device = "nitro.local",
 
     device = "http://"+device+"/";
 
+let features = {
+    drive: {
+        name: {
+            fr: "Drive",
+            en: "Drive"
+        },
+        desc: {
+            fr: "Hébergez vos fichiers localement sur votre réseau pour qu'ils soient accessible depuis tous les appareils connectés au réseau.",
+            en: "Host your files locally on your network to make them accessible drom all devices connected to this network."
+        },
+        icon: "M20 6h-8l-2-2H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm0 12H4V8h16v10z"
+    },
+
+    message: {
+        name: {
+            fr: "Messages",
+            en: "Messages"
+        },
+        desc: {
+            fr: "Discutez avec les utlisateurs connectées à votre réseau en envoyant des messages, photos, fichiers, podcasts, musiques, etc.",
+            en: "Talk with the users connected to your network with messages, pictures, files, podcasts, musics, and so on..."
+        },
+
+        iconSmall: "M20 2H4c-1.1 0-2 .9-2 2v18l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2z",
+        icon: "M4 4h16v12H5.17L4 17.17V4m0-2c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2H4zm2 10h12v2H6v-2zm0-3h12v2H6V9zm0-3h12v2H6V6z"
+    },
+
+    music: {
+        name: {
+            fr: "Musique",
+            en: "Music"
+        },
+        desc: {
+            fr: "Écoutez de la musique .",
+            en: "Talk with the users connected to your network with messages, pictures, files, podcasts, musics, and so on..."
+        },
+        icon: "M20 6h-8l-2-2H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm0 12H4V8h16v10z"
+    }
+};
+
 $("#nav .logo").on("enter", function() {
     $("path", this).removeClass("filled");
 
